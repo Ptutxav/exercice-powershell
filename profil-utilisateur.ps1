ff751c9187ae3e58528732ecf8bbfcd1bd492113
Ptutxav <97065891+Ptutxav@users.noreply.github.com>
2025-04-18 10:07:03 +0200

    ex 1

---
+++ profil-utilisateur.ps1
@@ -0,0 +1,6 @@
+﻿$nom= Read-Host "Quel est ton prénom et ton nom"
+$age= [int](Read-Host "Quel est ton âge")
+
+$naissance= (Get-Date).year - $age
+
+Write-Output "Enchanté $nom tu es née en $naissance"
