ce655567116ad90285d0b1975263ba2739b70bf3
Ptutxav <97065891+Ptutxav@users.noreply.github.com>
2025-04-18 12:01:31 +0200

    exo 5

---
+++ compteur-mots.ps1
@@ -0,0 +1,17 @@
+﻿#demande à l'utilisateur et enregistrement de la phrase
+$phrase= [String](Read-Host "Ecris une phrase de ton choix et je te renvoie le nombre de mots de celle ci")
+#initialisation d'un compteur
+#vérifier si la chaine est vide
+if ([string]::IsNullOrWhiteSpace($phrase)) {
+	#renvoie le resultat
+	Write-Output "Tu n'as pas écris de phrase !"
+} else {
+	# Découpe la phrase en mots, en supprimant les vides
+    $mots = $phrase.Split(" ", [System.StringSplitOptions]::RemoveEmptyEntries)
+    
+    # Compte les mots
+    $nbMots = $mots.Count
+	#renvoie le resultat
+	Write-Output "Ta phrase conttient $nbMots mots"
+}
+	
